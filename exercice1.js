f5f5c7c1f43fa01f83d8cf0e5a11c8eabdbdb728
NoelanL <noelan.lengrand@outlook.fr>
2021-11-17 22:58:56 +0100

    ..

--- exercice1.js
+++ exercice1.js
@@ -1,7 +1,16 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div e été ajouté en utilisant JavaScript";
+let premierDiv = document.getElementById('viewport').appendChild(monDiv);
+premierDiv.appendChild(monDiv);
 
-document.getElementById('viewport').appendChild(monDiv);
+let monDiv2 = document.createElement('div');
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.width = 100 + "px";
+monDiv2.style.height = 100 + "px";
+premierDiv.appendChild(monDiv2);
 
+let monSpan = document.getElementById('aSupprimer');
+monSpan.remove(premierDiv);
 
 
 
